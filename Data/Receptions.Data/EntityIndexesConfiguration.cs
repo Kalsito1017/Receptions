46e952607349d674bf4e79e6eb29a116920cffb5
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2025-06-06 18:23:02 +0300

    Fixed library sort

--- Data/Receptions.Data/EntityIndexesConfiguration.cs
+++ Data/Receptions.Data/EntityIndexesConfiguration.cs
@@ -2,9 +2,8 @@
 {
     using System.Linq;
 
-    using Receptions.Data.Common.Models;
-
     using Microsoft.EntityFrameworkCore;
+    using Receptions.Data.Common.Models;
 
     internal static class EntityIndexesConfiguration
     {
