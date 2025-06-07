4c1dc55438461d7644d1f1902168f3f028dea860
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2025-06-07 22:46:39 +0300

    Added new Seeder + IDeletableEntities

---
+++ Data/Receptions.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,25 @@
+﻿namespace Receptions.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Receptions.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Category.Any())
+            {
+                return;
+            }
+
+            await dbContext.Category.AddAsync(new Category { Name = "Пастърма" });
+            await dbContext.Category.AddAsync(new Category { Name = "Мусака" });
+            await dbContext.Category.AddAsync(new Category { Name = "Гръцка салата" });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
