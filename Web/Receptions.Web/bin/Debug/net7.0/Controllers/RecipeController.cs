9d8c27f72560169c5a5c4de1bcfa47e8d8e8e2c6
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2025-06-17 11:12:49 +0300

    Cache updated + made some API examples for post and get

--- Web/Receptions.Web/bin/Debug/net7.0/Controllers/RecipeController.cs
+++ Web/Receptions.Web/bin/Debug/net7.0/Controllers/RecipeController.cs
@@ -1,24 +1,50 @@
 ﻿namespace Receptions.Web.Controllers
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc;
+    using Receptions.Data;
     using Receptions.Data.Common.Repositories;
     using Receptions.Data.Models;
     using Receptions.Services.Data;
     using Receptions.Web.ViewModels.Home.ASPViewModels;
 
+    [Route("api/[controller]")]
+    [ApiController]
     public class RecipeController : Controller
     {
         private readonly ICategoryService categoryService;
         private readonly IRecipeService recipeService;
+        private readonly ApplicationDbContext db;
 
-        public RecipeController(ICategoryService categoryService, IRecipeService recipeService)
+        public RecipeController(ApplicationDbContext db)
         {
-            this.categoryService = categoryService;
-            this.recipeService = recipeService;
+            this.db = db;
         }
 
+        [HttpGet]
+        public IEnumerable<Recipe> Get()
+        {
+            return this.db.Recipes.ToList();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Recipe recipe)
+        {
+            await this.db.Recipes.AddAsync(recipe);
+            await this.db.SaveChangesAsync();
+            return this.CreatedAtAction("{GET}", new { Id = recipe.Id }, recipe);
+        }
+
+        // test message
+        // public RecipeController(ICategoryService categoryService, IRecipeService recipeService)
+        // {
+        //    this.categoryService = categoryService;
+        //    this.recipeService = recipeService;
+        // }
         public IActionResult Create()
         {
             var viewModel = new CreateRecipeInputModel();
