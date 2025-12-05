78f76f302598698888bca74c212caaaf56b6778b
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2025-12-05 22:38:54 +0200

    edit

--- Web/Receptions.Web/Controllers/BlogController.cs
+++ Web/Receptions.Web/Controllers/BlogController.cs
@@ -2,55 +2,58 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using Microsoft.AspNetCore.Mvc;
+    using Receptions.Data.Common.Repositories;
+    using Receptions.Data.Models;
 
     public class BlogController : Controller
     {
+        private readonly IDeletableEntityRepository<BlogPost> blogRepository;
+
+        public BlogController(IDeletableEntityRepository<BlogPost> blogRepository)
+        {
+            this.blogRepository = blogRepository;
+        }
+
         public IActionResult Index()
         {
-            var articles = new List<BlogPostViewModel>
-            {
-                new BlogPostViewModel
+            var articles = this.blogRepository.All()
+                .OrderByDescending(x => x.PublishedOn)
+                .Select(x => new BlogPostViewModel
                 {
-                    Id = 1,
-                    Title = "Тайната на перфектната шкембе чорба",
-                    Date = DateTime.Now.AddDays(-2),
-                    Author = "Калоян Стоичков",
-                    ImageUrl = "/images/shkembe.jpg",
-                    Excerpt = "Шкембе чорбата е класика в българската кухня. Научи тайната на добрия вкус...",
-                    Content = "...",
-                },
-                new BlogPostViewModel
-                {
-                    Id = 2,
-                    Title = "Как се приготвя традиционна баница",
-                    Date = DateTime.Now.AddDays(-5),
-                    Author = "Мария Петрова",
-                    ImageUrl = "/images/banitsa.jpg",
-                    Excerpt = "Истинската българска баница изисква внимание и любов към детайла...",
-                    Content = "...",
-                },
-            };
+                    Id = x.Id,
+                    Title = x.Title,
+                    Author = x.Author,
+                    Date = x.PublishedOn,
+                    ImageUrl = x.ImageUrl,
+                    Excerpt = x.Excerpt,
+                })
+                .ToList();
 
             return this.View(articles);
         }
 
         public IActionResult Details(int id)
         {
-            // Normally you'd fetch the post from a database; here’s dummy data:
-            var article = new BlogPostViewModel
+            var article = this.blogRepository.All()
+                .Where(x => x.Id == id)
+                .Select(x => new BlogPostViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Author = x.Author,
+                    Date = x.PublishedOn,
+                    ImageUrl = x.ImageUrl,
+                    Content = x.Content,
+                })
+                .FirstOrDefault();
+
+            if (article == null)
             {
-                Id = id,
-                Title = "Тайната на перфектната шкембе чорба",
-                Date = DateTime.Now.AddDays(-2),
-                Author = "Калоян Стоичков",
-                ImageUrl = "/images/shkembe.jpg",
-                Content = @"
-                    <p>Шкембе чорбата е едно от най-емблематичните ястия на българската кухня...</p>
-                    <p>Истинският вкус идва от правилно подготвеното шкембе и ароматния чеснов сос.</p>
-                ",
-            };
+                return this.NotFound();
+            }
 
             return this.View(article);
         }
