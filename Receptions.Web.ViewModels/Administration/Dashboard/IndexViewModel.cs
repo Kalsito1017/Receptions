c54a6e45328e62f88de7389ecd69823261a103a5
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2026-01-01 13:55:47 +0200

    Delete Receptions.Web.ViewModels directory

--- Receptions.Web.ViewModels/Administration/Dashboard/IndexViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Receptions.Web.ViewModels.Administration.Dashboard
-{
-    public class IndexViewModel
-    {
-        public int SettingsCount { get; set; }
-    }
-}
--- Receptions.Web.ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Receptions.Web.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
-    }
-}
