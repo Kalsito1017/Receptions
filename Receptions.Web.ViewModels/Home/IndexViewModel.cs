f0c4b52c75a0ca55fe9a41c2bc638417bc7ce4a8
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2026-01-01 13:55:06 +0200

    Added ViewModels for Home and receptions

---
+++ Receptions.Web.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Receptions.Data.Models;
+using Receptions.Web.ViewModels.Home;
+using Receptions.Web.ViewModels.Home.Recipe;
+
+public class IndexViewModel
+{
+    public int RecipesCount { get; set; }
+
+    public int CategoriesCount { get; set; }
+
+    public int IngredientsCount { get; set; }
+
+    public int ImagesCount { get; set; }
+
+    // Use only this list for the home page recipes
+    public List<RecipeItem> Recipes { get; set; } = new();
+
+    public List<RecipeImageViewModel> Images { get; set; } = new();
+
+    public List<RecipeIngredient> RecipeIngredients { get; set; } = new();
+
+    public List<Ingredient> Ingredients { get; set; } = new();
+
+    public List<int> SelectedIngredientIds { get; set; } = new();
+
+    public RecipeFilterInputModel Filters { get; set; }
+
+    public int PageNumber { get; set; }
+
+    public int TotalPages { get; set; }
+
+    public string SearchQuery { get; set; }
+
+    public IEnumerable<CategoryViewModel> Categories { get; set; }
+
+    public IEnumerable<RecipeIngredientInputModel> RecipeIngredientInputModels { get; set; }
+
+    // Nested class for each recipe in the home page
+    public class RecipeItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Category { get; set; }
+
+        public TimeSpan PrepTime { get; set; }
+
+        public TimeSpan CookTime { get; set; }
+
+        public int Servings { get; set; }
+
+        // For display
+        public List<string> Ingredients { get; set; } = new();
+
+        // For filtering - store ingredient IDs as comma-separated string
+        public string IngredientIds { get; set; }
+
+        public List<RecipeImageViewModel> Images { get; set; } = new();
+    }
+
+    public class RecipeImageViewModel
+    {
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; } // Base64 or URL
+    }
+}
---
+++ Receptions.Web.ViewModels/Home/Recipes/RecipeIngredientInputModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Receptions.Web.ViewModels.Home.Recipe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RecipeIngredientInputModel
+    {
+        [Required]
+        [MinLength(2)]
+        [DisplayName("Име на съставка")]
+        public string IngredientName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [DisplayName("Количество")]
+        public string Quantity { get; set; }
+    }
+}
