89fe9bab78b482aaedf1166a02bc4972a2188c47
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2026-01-01 14:09:50 +0200

    Added Services

--- Services/Receptions.Services.Data/IRecipeService.cs
+++ Services/Receptions.Services.Data/IRecipeService.cs
@@ -1,11 +1,15 @@
 ﻿namespace Receptions.Services.Data
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    using Microsoft.AspNetCore.Http;
     using Receptions.Web.ViewModels.Home.ASPViewModels;
 
     public interface IRecipeService
     {
-        Task CreateAsync(CreateRecipeInputModel input);
+        Task CreateAsync(CreateRecipeInputModel input, List<IFormFile> file);
+
+        Task<List<RecipeCarouselViewModel>> GetHomeCarouselRecipesAsync();
     }
 }
--- Services/Receptions.Services.Data/RecipeService.cs
+++ Services/Receptions.Services.Data/RecipeService.cs
@@ -1,16 +1,20 @@
 ﻿namespace Receptions.Services.Data;
 
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    using Receptions.Data.Common.Repositories;
-    using Receptions.Data.Models;
-    using Receptions.Web.ViewModels.Home.ASPViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Receptions.Data.Common.Repositories;
+using Receptions.Data.Models;
+using Receptions.Web.ViewModels.Home.ASPViewModels;
+using Receptions.Web.ViewModels.Home.Recipes;
 
-    public class RecipeService : IRecipeService
+public class RecipeService : IRecipeService
     {
         private readonly IDeletableEntityRepository<Recipe> recipeRepository;
 
@@ -22,9 +26,28 @@
         {
             this.recipeRepository = recipeRepository;
             this.ingredientRepository = ingredientRepository;
-        }
+    }
 
-        public async Task CreateAsync(CreateRecipeInputModel input)
+        public async Task<List<RecipeCarouselViewModel>> GetHomeCarouselRecipesAsync()
+    {
+        return await this.recipeRepository.All()
+            .Include(r => r.Images) // load related images
+            .OrderByDescending(r => r.CreatedOn)
+            .Select(r => new RecipeCarouselViewModel
+            {
+                Id = r.Id,
+                Name = r.Name,
+                Instructions = r.Instructions,
+                Images = r.Images.Select(i => new RecipeCarouselViewModel.ImageViewModel
+                {
+                    Name = i.Name,
+                    ImageUrl = "data:" + i.ContentType + ";base64," + Convert.ToBase64String(i.Data),
+                }).ToList(),
+            })
+            .ToListAsync();
+    }
+
+        public async Task CreateAsync(CreateRecipeInputModel input, List<IFormFile> uploadedImages)
     {
         var recipe = new Recipe()
         {
@@ -34,20 +57,45 @@
             Name = input.Name,
             PortionsCount = input.PortionsCount,
             PreparationTime = TimeSpan.FromMinutes(input.PreparationTime),
+            Images = new List<Image>(),
         };
+
         foreach (var inputIngredient in input.Ingredients)
         {
-           var ingredient = this.ingredientRepository.All().FirstOrDefault(x => x.Name == inputIngredient.IngredientName);
-           if (ingredient == null)
+            var ingredient = this.ingredientRepository.All()
+                .FirstOrDefault(x => x.Name == inputIngredient.IngredientName);
+
+            if (ingredient == null)
             {
                 ingredient = new Ingredient { Name = inputIngredient.IngredientName };
             }
 
-           recipe.Ingredients.Add(new RecipeIngredient
+            recipe.Ingredients.Add(new RecipeIngredient
             {
                 Ingredient = ingredient,
                 Quantity = inputIngredient.Quantity,
             });
+        }
+
+        // images (max 5)
+        if (uploadedImages != null && uploadedImages.Any())
+        {
+            foreach (var file in uploadedImages.Take(5))
+            {
+                if (file.Length > 0)
+                {
+                    using var ms = new MemoryStream();
+                    await file.CopyToAsync(ms);
+
+                    recipe.Images.Add(new Image
+                    {
+                        Name = Path.GetFileName(file.FileName),
+                        Data = ms.ToArray(),
+                        ContentType = file.ContentType,
+                        Extension = Path.GetExtension(file.FileName),
+                    });
+                }
+            }
         }
 
         await this.recipeRepository.AddAsync(recipe);
